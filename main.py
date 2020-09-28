f375e22eb31d4c3680283ae6985552adbe05e4ef
Ramón García <ramon.garcia5800.alumnos.udg.mx>
2020-09-28 14:22:55 -0500

    Prueba01

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Hola Mundo")
+base = int(input("Ingresa la base del triangulo: "))
+altura = int(input("Ingresa la altura del triangulo: "))
+print("El area del triangulo es: ", (base * altura)/2)
